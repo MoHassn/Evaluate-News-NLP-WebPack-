bfd3c2f579fe3aab1f91679f311e2097e11d0ff5
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2021-12-07 21:41:14 +0200

    optimize css in prod

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const loader = require("sass-loader");
 
 module.exports = {
   entry: "./src/client/index.js",
